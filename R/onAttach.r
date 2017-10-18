8bbb866ce9b47a0cb545658b4cadd2694bc21256
bioticinteractions <danssong@gmail.com>
2017-10-18 12:25:09 -0400

    last changes to formatting

--- R/onAttach.r
+++ R/onAttach.r
@@ -17,7 +17,6 @@
     # if (dev && (Sys.Date() - as.Date(d))>28)
       # packageStartupMessage("**********\nThis development version of data.table was built more than 4 weeks ago. Please update.\n**********")
     # if (!.Call(ChasOpenMP))
-      # packageStartupMessage("**********\nThis installation of data.table has not detected OpenMP support. It will still work but in single-threaded mode. If this a Mac and you obtained the Mac binary of data.table from CRAN, CRAN's Mac does not yet support OpenMP. In the meantime please follow our Mac installation instructions on the data.table homepage. If it works and you observe benefits from multiple threads as others have reported, please convince Simon Ubanek by sending him evidence and ask him to turn on OpenMP support when CRAN builds package binaries for Mac. Alternatives are to install Ubuntu on your Mac (which I have done and works well) or use Windows where OpenMP is supported and works well.\n**********")
     packageStartupMessage('  This is a function created by Daniel Song to improve workflow within R when using multiple models to make predictions.')
     packageStartupMessage('  This was tested with regresssion models but should work with classification models.')
     packageStartupMessage('  See ?predict2 for notes and tips that are likely to be of no use to you.')
